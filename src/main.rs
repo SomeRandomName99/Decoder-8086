aad104426163220d6c019c715f358402e05a11f3
SomeRandomName <husam.malkawi@hotmail.com>
2026-01-17 11:49:05 +0100

    Add imm to reg/mem and refactor code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fmt::Write;
 use std::fs;
 
+const W_BIT_MASK: u8 = 0b1;
 const REGISTER_MAP: [[&str; 2]; 8] = [
     ["al", "ax"],
     ["cl", "cx"],
@@ -68,64 +69,62 @@ fn decode_instructions(mut bytes: &[u8], arg1: &mut String, arg2: &mut String) {
                 decode_mov_mem_accu(&mut bytes, false);
                 continue 'decode;
             }
-            _ => panic!("Unsupported instruction. Opcode byte: {byte1:b}"),
+            0b1100011 => {
+                decode_mov_imm_regmem(&mut bytes, arg1, arg2);
+                continue 'decode;
+            }
+            _ => panic!(
+                "Unsupported instruction. Opcode byte: {byte1:#b}, {:#b}",
+                bytes[0]
+            ),
         }
     }
 }
 
-fn decode_mov_regmem_reg(bytes: &mut &[u8], arg1: &mut String, arg2: &mut String) {
-    const D_BIT_SHIFT: u8 = 1;
-    const D_BIT_MASK: u8 = 0b00000010;
-    const W_BIT_SHIFT: u8 = 0;
-    const W_BIT_MASK: u8 = 0b00000001;
+enum EffectiveAddress {
+    Reg(&'static str),                          // mod=0b11
+    Direct(u16),                                // rm=0b110 mod=0
+    Indirect { base: &'static str, disp: i16 }, // disp could be zero
+}
+
+// mod_bytes, rm, reg, displacement
+fn decode_effective_address_calculation(
+    bytes: &mut &[u8],
+    w_bit: usize,
+) -> (&'static str, EffectiveAddress) {
     const MOD_SHIFT: u8 = 6;
+    const RM_MASK: u8 = 0b000000111;
     const REG_SHIFT: u8 = 3;
     const REG_MASK: u8 = 0b00111000;
-    const RM_MASK: u8 = 0b000000111;
 
-    let byte1 = bytes[0];
+    let byte = bytes[0];
     *bytes = &bytes[1..];
-
-    let w_bit = ((byte1 & W_BIT_MASK) >> W_BIT_SHIFT) as usize;
-    let d_bit: bool = matches!((byte1 & D_BIT_MASK) >> D_BIT_SHIFT, 1);
-
-    let byte2 = bytes[0];
-    *bytes = &bytes[1..];
-    let mod_bytes = byte2 >> MOD_SHIFT;
-    let reg = ((byte2 & REG_MASK) >> REG_SHIFT) as usize;
+    let mod_bytes = byte >> MOD_SHIFT;
+    let r_m = (byte & RM_MASK) as usize;
+    let reg = ((byte & REG_MASK) >> REG_SHIFT) as usize;
     let reg_str: &str = REGISTER_MAP[reg][w_bit];
-    let r_m = (byte2 & RM_MASK) as usize;
 
-    let (dst, src) = match d_bit {
-        true => (&mut *arg1, &mut *arg2),
-        false => (&mut *arg2, &mut *arg1),
-    };
     if mod_bytes == 0b11 {
-        let rm_reg_str: &str = REGISTER_MAP[r_m][w_bit];
-
-        dst.push_str(reg_str);
-        src.push_str(rm_reg_str);
+        (reg_str, EffectiveAddress::Reg(REGISTER_MAP[r_m][w_bit]))
     } else {
-        let rm_reg_str = match r_m {
-            0b000 => "bx + si",
-            0b001 => "bx + di",
-            0b010 => "bp + si",
-            0b011 => "bp + di",
-            0b100 => "si",
-            0b101 => "di",
-            0b110 => "bp",
-            0b111 => "bx",
-            _ => panic!("Invalid R/M"),
-        };
-
-        write!(dst, "{reg_str}").unwrap();
         // Direct address mode
         if r_m == 0b110 && mod_bytes == 0 {
             let address: u16 = u16::from_le_bytes([bytes[0], bytes[1]]);
             *bytes = &bytes[2..];
 
-            write!(src, "[{address}]").unwrap();
+            (reg_str, EffectiveAddress::Direct(address))
         } else {
+            let rm_reg_str = match r_m {
+                0b000 => "bx + si",
+                0b001 => "bx + di",
+                0b010 => "bp + si",
+                0b011 => "bp + di",
+                0b100 => "si",
+                0b101 => "di",
+                0b110 => "bp",
+                0b111 => "bx",
+                _ => panic!("Invalid R/M"),
+            };
             let mut displacement: i16 = 0;
             if mod_bytes == 0b01 {
                 displacement = (bytes[0] as i8) as i16;
@@ -134,10 +133,81 @@ fn decode_mov_regmem_reg(bytes: &mut &[u8], arg1: &mut String, arg2: &mut String
                 displacement = i16::from_le_bytes([bytes[0], bytes[1]]);
                 *bytes = &bytes[2..];
             }
-            if displacement != 0 {
-                write!(src, "[{} + {}]", rm_reg_str, displacement).unwrap();
+            (
+                reg_str,
+                EffectiveAddress::Indirect {
+                    base: rm_reg_str,
+                    disp: displacement,
+                },
+            )
+        }
+    }
+}
+
+fn decode_mov_imm_regmem(bytes: &mut &[u8], dst: &mut String, src: &mut String) {
+    let byte1 = bytes[0];
+    *bytes = &bytes[1..];
+    let w_bit = (byte1 & W_BIT_MASK) as usize;
+
+    let (_, eff_add) = decode_effective_address_calculation(bytes, w_bit);
+    match eff_add {
+        EffectiveAddress::Reg(rm_reg_str) => {
+            dst.push_str(rm_reg_str);
+        }
+        EffectiveAddress::Direct(address) => {
+            write!(dst, "[{address}]").unwrap();
+        }
+        EffectiveAddress::Indirect { base, disp } => {
+            if disp != 0 {
+                write!(dst, "[{} + {}]", base, disp).unwrap();
             } else {
-                write!(src, "[{}]", rm_reg_str).unwrap();
+                write!(dst, "[{}]", base).unwrap();
+            }
+        }
+    }
+
+    if w_bit == 1 {
+        let immediate = i16::from_le_bytes([bytes[0], bytes[1]]);
+        *bytes = &bytes[2..];
+        write!(src, "word {immediate}").unwrap();
+    } else {
+        let immediate = (bytes[0] as i8) as i16;
+        *bytes = &bytes[1..];
+        write!(src, "byte {immediate}").unwrap();
+    }
+
+    println!("mov {dst} {src}");
+}
+
+fn decode_mov_regmem_reg(bytes: &mut &[u8], arg1: &mut String, arg2: &mut String) {
+    const D_BIT_SHIFT: u8 = 1;
+    const D_BIT_MASK: u8 = 0b00000010;
+
+    let byte1 = bytes[0];
+    *bytes = &bytes[1..];
+
+    let w_bit = (byte1 & W_BIT_MASK) as usize;
+    let d_bit: bool = matches!((byte1 & D_BIT_MASK) >> D_BIT_SHIFT, 1);
+
+    let (dst, src) = match d_bit {
+        true => (&mut *arg1, &mut *arg2),
+        false => (&mut *arg2, &mut *arg1),
+    };
+
+    let (reg_str, eff_add) = decode_effective_address_calculation(bytes, w_bit);
+    dst.push_str(reg_str);
+    match eff_add {
+        EffectiveAddress::Reg(rm_reg_str) => {
+            src.push_str(rm_reg_str);
+        }
+        EffectiveAddress::Direct(address) => {
+            write!(src, "[{address}]").unwrap();
+        }
+        EffectiveAddress::Indirect { base, disp } => {
+            if disp != 0 {
+                write!(src, "[{} + {}]", base, disp).unwrap();
+            } else {
+                write!(src, "[{}]", base).unwrap();
             }
         }
     }
@@ -146,6 +216,7 @@ fn decode_mov_regmem_reg(bytes: &mut &[u8], arg1: &mut String, arg2: &mut String
 }
 
 fn decode_mov_imm_reg(bytes: &mut &[u8]) {
+    // This is the only instruction with the w bit not at the end of the opcode byte
     const W_BIT_MASK: u8 = 0b00001000;
     const W_BIT_SHIFT: u8 = 3;
     const REG_MASK: u8 = 0b00000111;
